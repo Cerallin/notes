44f3f61d9e0da6b9fbb870f88036172bc193f4da
Cerallin <cerallin@cerallin.top>
2023-09-21 22:10:55 +0800

    Site updated: 2023-09-21 22:10:54

--- js/theme.js
+++ js/theme.js
@@ -182,7 +182,7 @@
           button.addEventListener('click', this.openSearchModal(modal)));
     },
     registerSearchBox(modal) {
-      const closeModal = debounce(this.closeSearchModal(modal), 100);
+      const closeModal = this.closeSearchModal(modal);
       // close button
       [].forEach.call(document.getElementsByClassName('close-button'),
         button => button.addEventListener('click', closeModal));
@@ -192,7 +192,6 @@
       const inputElement = document.getElementById('search-input');
       inputElement.addEventListener('keyup',
         ({ key }) => {
-          console.log(key)
           if (key === "Enter") searchFunc();
           else if (key === "Escape") closeModal();
         })
@@ -269,6 +268,52 @@
           console.error(err);
         })
     },
+  };
+
+  Theme.toc = {
+    register() {
+      const tocWrapper = document.getElementById('post-toc');
+      const sections = document.querySelectorAll('article h3[id], article h4[id], article h5[id]');
+
+      // scroll title to the middle of toc wrapper, and deactivate all the titles
+      function scrollToMiddle(node) {
+        const firstChild = node.firstElementChild;
+        // if the node contains a title and a list of subtitles, take the title instead
+        if (firstChild && firstChild.tagName === 'A')
+          node = firstChild;
+        // scroll the tocWrapper to middle
+        const scrollTop = node.offsetTop - (tocWrapper.clientHeight - node.clientHeight) / 2;
+        tocWrapper.scrollTop = scrollTop;
+        // reset all "data-actives"
+        [].map.call(sections,
+          function (item) {
+            const id = item.getAttribute('id');
+            const parentNode = document.querySelector(
+              `.post-toc-item a[href="#${encodeURIComponent(id)}"]`)
+              .parentNode;
+            console.log(id)
+            parentNode.removeAttribute('data-active');
+          })
+      }
+
+      // Track all sections
+      const observer = new IntersectionObserver(debounce(function (entries) {
+        entries.forEach(entry => {
+          const id = entry.target.getAttribute('id');
+          const parentNode = document.querySelector(
+            `.post-toc-item a[href="#${encodeURIComponent(id)}"]`)
+            .parentNode;
+
+          if (entry.intersectionRatio > 0) {
+            scrollToMiddle(parentNode);
+            parentNode.setAttribute('data-active', '');
+          } else {
+            parentNode.removeAttribute('data-active');
+          }
+        })
+      }, 100));
+      sections.forEach((section) => observer.observe(section));
+    }
   };
 
   this.Theme = Theme;
